c1272274df926a98c31184f99f240850b62efb96
samuFerragut <sferragut@cifpfbmoll.eu>
2020-02-11 10:02:36 +0100

    feat(Dni, DniTest): Crear casos test + lógica

--- src/main/java/edu/elsmancs/domain/Dni.java
+++ src/main/java/edu/elsmancs/domain/Dni.java
@@ -1,4 +1,37 @@
 package edu.elsmancs.domain;
 
 public class Dni {
+    private String dni;
+
+    private TablaAsignacion tabla = new TablaAsignacion();
+
+    public Dni(String dni) {
+        this.dni = dni;
+    }
+
+    private String getDni() {
+        return this.dni;
+    }
+
+    private TablaAsignacion getTablaAsignacion() {
+        return this.tabla;
+    }
+
+    @Override
+    public String toString() {
+        return getDni();
+    }
+
+    Boolean isDniNumero(String cadena) {
+        for (int i = 0; i < cadena.length(); i++) {
+            if (!Character.isDigit(cadena.charAt(i))) {
+                return false;
+            } else
+                ;
+        }
+        return true;
+    }
+    String extraerParteNumericaDni() {
+        return (String) dni.substring(0, dni.length() - 1);
+    }
 }
--- src/test/java/edu/elsmancs/domain/DniTest.java
+++ src/test/java/edu/elsmancs/domain/DniTest.java
@@ -1,4 +1,25 @@
 package edu.elsmancs.domain;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class DniTest {
+
+    private static final char[] letrasNoPermitidas = {'I', 'Ñ', 'O', 'U'};
+
+    private static final String[] casosTestPass = { //casos OK
+            "78484464T", "72376173A", "01817200Q", "95882054E", "63587725Q",
+            "26861694V", "21616083Q", "26868974Y", "40135330P", "89044648X",
+            "80117501Z", "34168723S", "76857238R", "66714505S", "66499420A"};
+
+    @Test
+    public void getParteNumericaDni_stringEsNumero() {
+        Dni objetoDniCif = null;
+        for (String dni : casosTestPass) {
+            objetoDniCif = new Dni(dni);
+            assertTrue(objetoDniCif.isDniNumero(
+                    objetoDniCif.extraerParteNumericaDni()));
+        }
+    }
 }
